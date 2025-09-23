077fe10478b62ff91775d55b3102407fc6a84509
grazielac <cpgraziela@gmail.com>
2025-09-23 12:00:30 +0200

    set up env variables

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+// import appears "lighter" when they're currently unused
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("This is the root route!");
+});
+
+app.listen(8080, () => {
+    console.log(`Server running on port 8080`);
+});
