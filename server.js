cc75b2bd223257472e65ef05f042d0423da64d6f
grazielac <cpgraziela@gmail.com>
2025-09-23 12:18:17 +0200

    add env config for available use

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// calling this always at the beginning, to ensure that the
+// variables defined in .env file are available for use
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("This is the root route!");
+  res.send("This is the root route!");
+});
+
+app.get("/images", async (req, res) => {
+  const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=puppy`;
+  const response = await fetch(API);
+  const imageData = await response.json();
+
+  res.json(imageData.results); // result is a property?
 });
 
 app.listen(8080, () => {
-    console.log(`Server running on port 8080`);
+  console.log(`Server running on port 8080`);
 });
