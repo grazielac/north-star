24327b8a0cafdd8a33702e5f312dc26cc91c5dc8
grazielac <cpgraziela@gmail.com>
2025-09-24 21:55:52 +0200

    add start script server side

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.json());
 app.use(cors());
